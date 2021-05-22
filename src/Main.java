8cc20451147b8b08d9dfe086e2890bc3bd7cee1f
Olivier Lefebvre-Montembault <montembault.olivier@gmail.com>
2021-05-22 18:35:02 -0400

    Update Main.java
    Ajustement pour travail noté

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,18 @@ public class Main {
         int retour = 0;
         for(int iter = 1; iter <= 10000; iter++) {
             if (iter%3 != 0) {
-                System.out.println(iter + " n'est pas divisible par 3.");
-                if( Integer.toString(iter).length() <= 2) {
-                    System.out.print(retour + " + " + iter);
+                String nombre = Integer.toString(iter);
+                if( nombre.length() <= 2) {
                     retour += iter;
-                    System.out.println(" = " + retour);
                 } else {
-                    String nombre = Integer.toString(iter);
                     int index = nombre.length() - 3;
-                    if (Character.getNumericValue(nombre.charAt(index)) != 2 && Character.getNumericValue(nombre.charAt(index)) != 3) {
-                        System.out.print(retour + " + " + iter);
+                    int compare = Character.getNumericValue(nombre.charAt(index));
+                    if (compare != 2 && compare != 3) {
                         retour += iter;
-                        System.out.println(" = " + retour);
-                    } else {
-                        System.out.println("Le nombre " + nombre + " ne sera pas additionné.");
                     }
                 }
             }
         }
-        System.out.println("Le total est de :");
-        System.out.println(retour);
+        System.out.println("Le total est de : " + retour);
     }
 }
